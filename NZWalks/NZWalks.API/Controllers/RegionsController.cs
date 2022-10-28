47cb15c6363b33485e77dcddec2fb7943c41081f
abinbs <abinbinusam@gmail.com>
2022-10-28 09:53:44 +0530

    region controller added

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        public readonly IMapper mapper;
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+        [HttpGet]
+        public async  Task<IActionResult> GetAllRegions()
+        {
+
+
+
+            //var regions = new List<Region>()
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Wellington",
+            //        Code = "WLG",
+            //        Area = 45254,
+            //        Lat = -1.2564,
+            //        Long = 895.2645,
+            //        Population = 23152
+            //    },
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Auckland",
+            //        Code = "AUCK",
+            //        Area = 12451,
+            //        Lat = -3.2564,
+            //        Long = 95.2645,
+            //        Population = 33152
+            //    },
+            //};
+
+            var regions = await regionRepository.GetAllAsync();
+
+            //return DTO regions
+            //var regionsDTO = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(region =>
+            //{
+            //    var regionDTO = new Models.DTO.Region()
+            //    {
+            //        Id = region.Id,
+            //        Name = region.Name,
+            //        Code = region.Code,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population,
+            //    };
+            //    regionsDTO.Add(regionDTO);
+            //});
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+            return Ok(regionsDTO);
+        }
+    }
+}
